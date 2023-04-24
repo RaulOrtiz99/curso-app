f4fd48bb20dfa6d262c2a1418e69a58ddf024144
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2023-04-24 02:44:39 -0400

    agregando rutas basicas en este proyecto para ver por donde sale el sol con laravel

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/games',function(){
+    return "Bienvenido a la web que listara los juegos comprados";
+});
+
+Route::get('/games/create',function(){
+    return "Esta es la pagina donde se creara el formulario para dar de alta juegos";
+});
+
+Route::get('/games/{name_game}/{categoria?}',function($name_game,$categoria=null){
+    if($categoria){
+        return "Bienvenido a la pagina del juego:" .$name_game. "que pertenece a la categoria :" .$categoria;
+    }else{
+        return "Bienvenido a la pagina del juego:".$name_game;
+    }
 });
